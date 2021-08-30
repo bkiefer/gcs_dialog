4d11321bd8667352710629a8bd59eb8403171ac4
Christian Willms <christian.willms@dfki.de>
2021-08-30 10:51:06 +0200

    added basic rosnode implementations

---
+++ speech_processing_ros/speech_processing/scripts/skeleton_listener.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from rospy_message_converter import message_converter
+import socket
+import os
+import json
+from gcs_msgs.msg import Skeleton
+
+
+class AsrListener:
+    # Connect the socket to the port where the server is listening
+    server_address = (os.environ.get("DIA_IP"), 11666)
+    BUFFER_SIZE = 20
+
+    def callback(self, data):
+        if data.transcript[0] != "":
+            # open socket and send data
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect(self.server_address)
+            try:
+                dictionary = message_converter.convert_ros_message_to_dictionary(
+                    data)
+                dictionary["type"] = "Skeleton"
+                sock.sendall(json.dumps(dictionary))
+            finally:
+                sock.close()
+
+    def __init__(self):
+        rospy.Subscriber("skeleton",
+                         Skeleton, self.callback)
+
+
+if __name__ == '__main__':
+    rospy.logdebug("starting skeleton_listener")
+    rospy.init_node("skeleton_listener")
+    bridge = AsrListener()
+    rospy.spin()
