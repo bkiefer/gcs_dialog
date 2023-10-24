483e3bb5777f0e9e4890d93d177e5492b2fb9add
Bernd Kiefer <bernd.kiefer@dfki.de>
2023-10-24 12:40:15 +0200

    Fix starting docker instead of locally.

---
+++ start_docker.sh
@@ -0,0 +1,6 @@
+if test -z "$NETWORK_NAME"; then
+    NETWORK_NAME=docker0
+fi
+DOCKER_HOST=$(ip -4 addr show $NETWORK_NAME| grep -Po 'inet \K[\d.]+')
+sed 's/\( *brokerhost:\).*/\1 '"$DOCKER_HOST/" config.yml > docker_config.yml
+docker run --restart always -d -v `pwd`/docker_config.yml:/config.yml --name gcs_dialog_manager drz/gcs_dialog_manager:latest
